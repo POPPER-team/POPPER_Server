12b7ce1cd10b148e8608d688b1e612eac8a3521a
Fran <cvok.fran@gmail.com>
2024-03-28 15:34:57 +0100

    Removed bad port in launchSettings.json for https

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,39 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MySql.Data.MySqlClient;
 using POPPER_Server.Dtos;
 using POPPER_Server.Services;
 
 namespace POPPER_Server.Controllers;
+
 [Route("api/[controller]")]
 public class TestController : ControllerBase
 {
-    private readonly IMongoDatabase _database;
-    private readonly IMinioService  _minioService;
-    public TestController(IMongoDatabase database, IMinioService minioService)
+    private readonly IMongoDatabase _userDatabase;
+    private readonly IMinioService _minioService;
+    private readonly MySqlConnection _mySqlConnection;
+
+    public TestController(IMongoDatabase userDatabase, IMinioService minioService, MySqlConnection mySqlConnection)
     {
-        _database = database;
+        _userDatabase = userDatabase;
         _minioService = minioService;
+        _mySqlConnection = mySqlConnection;
     }
+
     [HttpGet("[action]")]
     public IActionResult GetUser()
     {
-        var collection = _database.GetCollection<BsonDocument>("testCollection");
+        var collection = _userDatabase.GetCollection<BsonDocument>("testCollection");
         var document = collection.Find(new BsonDocument());
         var dictionary = document.ToList().Select(x => x.ToDictionary());
         return Ok(dictionary);
     }
+
     [HttpPost("[action]")]
     public IActionResult PostUser(string postString)
     {
-        var collection = _database.GetCollection<BsonDocument>("testCollection");
+        var collection = _userDatabase.GetCollection<BsonDocument>("testCollection");
         collection.InsertOne(new BsonDocument("test", postString));
         return Ok();
     }
+
     [HttpPost("[action]")]
     public async Task<IActionResult> UploadFile([FromForm] FileUploadDto file)
     {
-        
         var filePath = Path.GetTempFileName();
 
         using (var stream = new FileStream(filePath, FileMode.Create))
@@ -45,6 +52,7 @@ public class TestController : ControllerBase
 
         return Ok($"File {file.File.FileName} uploaded successfully.");
     }
+
     [HttpGet("[action]")]
     public async Task<IActionResult> DownloadFile(string fileName)
     {
@@ -52,9 +60,16 @@ public class TestController : ControllerBase
         await _minioService.DownloadFileAsync("test-bucker", fileName, filePath);
         return PhysicalFile(filePath, "application/octet-stream", fileName);
     }
+
     [HttpGet("[action]")]
     public async Task<IActionResult> ListFiles()
     {
         return Ok(await _minioService.GetListFilesAsync("test-bucker"));
+    }
+
+    [HttpGet("[action]")]
+    public async Task<bool> DatabaseList()
+    {
+        return await _mySqlConnection.PingAsync();
     }
 }
