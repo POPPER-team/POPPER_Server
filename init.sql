0f673ed539e2cc5b90f426eb24656cbcd98768ec
rpetric98 <rpetric@algebra.hr>
2024-04-09 11:01:37 +0200

    Added user table

--- init.sql
+++ init.sql
@@ -13,3 +13,35 @@ CREATE TABLE IF NOT EXISTS tableData (
 
 -- Insert data into the table
 INSERT INTO tableData (name) VALUES ('Data 1'), ('Data 2'), ('Data 3');
+
+create database if not exists useri;
+       
+use useri;
+    
+    
+create table if not exists Users(
+    id int auto_increment,
+    FirstName varchar(255),
+    LastName varchar(255),
+    DateOfBirth date,
+    Username varchar(255),
+    Password varchar(255),
+    Created date,
+    Status varchar(255),
+    WebLink varchar(255),
+    PreferedUnits varchar(255),
+    Language varchar(255),
+    
+    
+    Primary key(id)
+    );
+
+create table if not exists Following(
+    id int auto_increment,
+    UserID int,
+    FollowingID int,
+    Primary key(id)
+    foreign key(UserID) references Users(id),
+    foreign key(FollowingID) references Users(id)
+    );  
+);
