4ed6660397c9fe4b8303cdd7bfb7e455d61e2e24
rpetric98 <rpetric@algebra.hr>
2024-04-14 20:02:32 +0200

    Splitted into two controllers UserAuth and UserDet

--- Controllers/UserAuthenticationController.cs
+++ Controllers/UserAuthenticationController.cs
@@ -7,13 +7,12 @@ using POPPER_Server.Services;
 
 namespace POPPER_Server.Controllers;
 [Route("api/[controller]")]
-public class UserController : ControllerBase
+public class UserAuthenticationController : ControllerBase
 {
-    //TODO split into two controllers UserAuthenticationController and UserDetailsController
     private readonly IUserServices _userServices;
     private readonly IMapper _mapper;
 
-    public UserController(IUserServices userServices, IMapper mapper)
+    public UserAuthenticationController(IUserServices userServices, IMapper mapper)
     {
         _userServices = userServices;
         _mapper = mapper;
@@ -30,16 +29,5 @@ public class UserController : ControllerBase
         await _userServices.RegisterUserAsync(userDto); 
         return Ok();
     }
-    [HttpGet("[action]/{userGuid}")]
-    public async Task<IActionResult> GetUser([FromRoute] string userGuid)
-    {
-        return Ok(_mapper.Map<UserDetailsDto>(await _userServices.GetUserAsync(userGuid)));
-    }
-    //TODO remove when we add more Authorized enpoints Now it is used for testing
-    [Authorize]
-    [HttpGet("[action]")]
-    public async Task<IActionResult> SearchUser(string searchString)
-    {
-        return Ok(_mapper.Map<List<UserDto>>(await _userServices.SearchUserAsync(searchString)));
-    }
+
 }
