cca7d22585e575f6ec1a3da17d3399cb70bc8766
Tiitmouse <lkovacic380@gmail.com>
2024-04-20 09:26:09 +0200

    Minor fixes No refresh token

--- Controllers/UserDetailsController.cs
+++ Controllers/UserDetailsController.cs
@@ -10,14 +10,20 @@ public class UserDetailsController: ControllerBase
 {
     private readonly IUserServices _userServices;
     private readonly IMapper _mapper;
+
+    public UserDetailsController(IUserServices userServices, IMapper mapper)
+    {
+        _userServices = userServices;
+        _mapper = mapper;
+    }
     
+    [Authorize]
     [HttpGet("[action]/{userGuid}")]
     public async Task<IActionResult> GetUser([FromRoute] string userGuid)
     {
         return Ok(_mapper.Map<UserDetailsDto>(await _userServices.GetUserAsync(userGuid)));
     }
     //TODO remove when we add more Authorized enpoints Now it is used for testing
-    [Authorize]
     [HttpGet("[action]")]
     public async Task<IActionResult> SearchUser(string searchString)
     {
