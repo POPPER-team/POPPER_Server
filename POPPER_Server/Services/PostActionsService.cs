f6d9558f93d776bdd3a61d91e2b30cbe28e5a01f
killi1812 <cvok.fran@gmail.com>
2024-07-01 15:48:48 +0200

    Function definitions

--- POPPER_Server/Services/PostActionsService.cs
+++ POPPER_Server/Services/PostActionsService.cs
@@ -1,3 +1,39 @@
-public interface IPostActions { }
+using POPPER_Server.Models;
 
-public class PostActions : IPostActions { }
+public interface IPostActions
+{
+    public Task<int> LikePost(string guid);
+    public Task<List<Comment>> PostComment(string guid);
+    public Task<int> SavePost(string guid);
+    public Task<string> GetShareLink(string guid);
+}
+
+public class PostActions : IPostActions
+{
+    private readonly PopperdbContext _context;
+
+    public PostActions(PopperdbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<string> GetShareLink(string guid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> LikePost(string guid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Comment>> PostComment(string guid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> SavePost(string guid)
+    {
+        throw new NotImplementedException();
+    }
+}
